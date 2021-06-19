e20c093b276a55ba82cac8455a80c39694757983
nir <nirsarkarpintwo@gmail.com>
2021-06-19 14:50:10 +0600

    Add files via upload

---
+++ start_miner.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETH
+#
+
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+./PhoenixMiner -pool ethash.unmineable.com:3333 -wal SHIB:0x26e174dC8FF20831EAA75fEf12822e371AD662e7.nir -pass x
---
+++ start_miner_etc.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETC
+#
+
+# IMPORTANT: Replace the ETC address with your own ETC wallet address in the -wal option (Rig001 is the name of the rig)
+./PhoenixMiner -pool ethash.unmineable.com:3333 -wal SHIB:0x26e174dC8FF20831EAA75fEf12822e371AD662e7.nir -pass x
